95b0c4e9055d664bf999cdf54349336364a7c22e
MaskdDev <maskddev@gmail.com>
2026-02-07 00:50:31 +1100

    Added POST /rooms/:roomId/messages route

--- routes/messages.ts
+++ routes/messages.ts
@@ -4,7 +4,8 @@ import type { Request } from "express";
 import { isMember } from "../queries/members.ts";
 import { roomExists } from "../queries/rooms.ts";
 import { roomNotFound } from "../utils/responses.ts";
-import { getMessages } from "../queries/messages.ts";
+import { createMessage, getMessages } from "../queries/messages.ts";
+import type { MessageCreate } from "../utils/types.ts";
 
 // Create router for route group
 const router = Router({ mergeParams: true });
@@ -64,6 +65,52 @@ router.get("/", async (req: MergedRequest, res) => {
 
     // Return messages
     res.status(200).json({ messages });
+  }
+});
+
+/**
+ * Send a new message in a given room.
+ */
+router.post("/", async (req: MergedRequest, res) => {
+  // Get user from request
+  const user = req.authUser;
+
+  // Check if user is authenticated
+  if (user) {
+    // Get room ID
+    const roomId = BigInt(req.params.roomId);
+
+    // Check if room exists
+    if (!(await roomExists(roomId))) {
+      return roomNotFound(res);
+    }
+
+    // Check if user is in room.
+    const inRoom = await isMember(user.id, roomId);
+    if (!inRoom) {
+      return res.status(403).json({
+        code: 403,
+        message:
+          "User does not have the permission to send messages in this channel.",
+      });
+    }
+
+    // Get request body
+    const body: MessageCreate = req.body;
+
+    // Check message length
+    if (!body.content || body.content.length > 1000) {
+      return res.status(422).json({
+        code: 422,
+        message: "Message content must be between 1 and 1000 characters long.",
+      });
+    }
+
+    // Send message
+    const message = await createMessage(roomId, user.id, body.content);
+
+    // Return message
+    res.status(200).json(message);
   }
 });
 
